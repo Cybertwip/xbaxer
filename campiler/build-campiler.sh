953465a8354b199283cdfba89efd7b6c1f7ca5ac
cybertwip <cybertwip@gmail.com>
2026-04-17 21:46:02 -0600

    Campiler

--- campiler/build-campiler.sh
+++ campiler/build-campiler.sh
@@ -10,6 +10,8 @@ GO_SOURCE_DIR="$1"
 BOOTSTRAP_GOROOT="$2"
 WORK_DIR="$3"
 OUTPUT_DIR="$4"
+TARGET_GOOS="windows"
+TARGET_GOARCH="amd64"
 
 rm -rf "$WORK_DIR" "$OUTPUT_DIR"
 mkdir -p "$(dirname "$WORK_DIR")" "$(dirname "$OUTPUT_DIR")"
@@ -22,19 +24,20 @@ chmod +x \
   "$WORK_DIR/src/bootstrap.bash"
 
 pushd "$WORK_DIR/src" >/dev/null
-GOROOT_BOOTSTRAP="$BOOTSTRAP_GOROOT" GOOS=windows GOARCH=amd64 bash ./make.bash --no-banner
+GOROOT_BOOTSTRAP="$BOOTSTRAP_GOROOT" GOOS="$TARGET_GOOS" GOARCH="$TARGET_GOARCH" bash ./make.bash --no-banner
 
 gohostos="$(../bin/go env GOHOSTOS)"
 gohostarch="$(../bin/go env GOHOSTARCH)"
-goos="$(../bin/go env GOOS)"
-goarch="$(../bin/go env GOARCH)"
 popd >/dev/null
 
-if [[ "$goos" != "$gohostos" || "$goarch" != "$gohostarch" ]]; then
-  rm -f "$WORK_DIR/bin/go_${goos}_${goarch}_exec"
-  if [[ -d "$WORK_DIR/bin/${goos}_${goarch}" ]]; then
-    mv "$WORK_DIR/bin/${goos}_${goarch}"/* "$WORK_DIR/bin/"
-    rmdir "$WORK_DIR/bin/${goos}_${goarch}"
+if [[ "$TARGET_GOOS" != "$gohostos" || "$TARGET_GOARCH" != "$gohostarch" ]]; then
+  rm -f \
+    "$WORK_DIR/bin/go" \
+    "$WORK_DIR/bin/gofmt" \
+    "$WORK_DIR/bin/go_${TARGET_GOOS}_${TARGET_GOARCH}_exec"
+  if [[ -d "$WORK_DIR/bin/${TARGET_GOOS}_${TARGET_GOARCH}" ]]; then
+    mv "$WORK_DIR/bin/${TARGET_GOOS}_${TARGET_GOARCH}"/* "$WORK_DIR/bin/"
+    rmdir "$WORK_DIR/bin/${TARGET_GOOS}_${TARGET_GOARCH}"
   fi
   rm -rf "$WORK_DIR/pkg/${gohostos}_${gohostarch}" "$WORK_DIR/pkg/tool/${gohostos}_${gohostarch}"
 fi
